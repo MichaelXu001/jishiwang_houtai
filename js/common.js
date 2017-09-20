7cf75e3f9b7074ccb6c04a15cc491c46021ff756
MichaelXu <13240368231@163.com>
2017-09-20 18:46:44 +0800

    第一次提交

---
+++ js/common.js
@@ -0,0 +1,50 @@
+function Common() {
+    this.ajaxFun = function(url, types, option, callback, asyncType) { //  路径，数据，正确的回调，
+            var async = asyncType ? asyncType : true;
+            $.ajax({
+                url: url,
+                type: types,
+                dataType: "json",
+                data: option,
+                async: async,
+                success: function(rs) {
+                    if ($.isFunction(callback)) callback(rs)
+                },
+                error: function(rs) {
+                    console.log(rs)
+                }
+            })
+        },
+        this.Geturl = function(name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+            var r = window.location.search.substr(1).match(reg);
+            if (r != null) return unescape(r[2]);
+            return null;
+        },
+        this.showOrHide = function(res) {
+            $("#telescopic").click(function() {
+                $(this).toggleClass("shrink");
+                $("#leftMenu").toggleClass("shrinkMenu");
+                $("#logo").toggleClass("shrinklogo");
+                $("#rightMain").toggleClass("shrinkrightMain")
+            })
+        },
+        this.setCookie = function(name, value, days) {
+            var date = new Date().getTime();
+            days ? days : 30;
+            date += days * 24 * 60 * 60 * 1000;
+            document.cookie = name + "=" + escape(value) + ";path=/;expires=" + new Date(date).toUTCString();
+        },
+        this.getCookie = function(name) {
+            var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+            if (arr != null) return unescape(arr[2]);
+            return "";
+        },
+
+        this.removeCookie = function(name) {
+            var date = new Date().getTime() - 1;
+            document.cookie = name + "=" + escape(name) + ";expires=" + new Date(date).toUTCString();
+        }
+}
+common = new Common();
+common.showOrHide(); //左边导航展开和收缩
